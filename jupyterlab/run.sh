c45153a55c9cd291729d19ecb8ba5ae2c412428a
cn47 <cn47@DESKTOP-B5HOTKT.localdomain>
2023-02-05 01:17:34 +0900

    add: ptpython config to jupyterlab

--- jupyterlab/run.sh
+++ jupyterlab/run.sh
@@ -7,14 +7,16 @@ export USER=`whoami`
 export PORT=8889
 export MOUNT_HOST_HOME="${HOME}:/home/host"
 export MOUNT_JUPYTER_HOME="${jupyter_home_path}:/home"
-export MOUNT_JUPYTER_USER_SETTINGS="./jupyter_user_settings:/home/${USER}/.jupyter/lab/user-settings"
-export MOUNT_JUPYTER_NOTEBOOK_SETTINGS="./jupyter_notebook_config.py:/home/${USER}/.jupyter/jupyter_notebook_config.py"
-export MOUNT_JUPYTER_NOTEBOOK_SETTINGS="./ipython_config.py:/home/${USER}/.ipython/profile_default/ipython_config.py"
+export MOUNT_JUPYTER_USER_SETTINGS="./settings/jupyter_user_settings:/home/jupyter/.jupyter/lab/user-settings"
+export MOUNT_JUPYTER_NOTEBOOK_SETTINGS="./settings/jupyter_notebook_config.py:/home/jupyter/.jupyter/jupyter_notebook_config.py"
+export MOUNT_IPYTHON_SETTINGS="./settings/ipython_config.py:/home/jupyter/.ipython/profile_default/ipython_config.py"
+export MOUNT_PTPYTHON_SETTINGS="./settings/ptpython_config.py:/home/jupyter/.config/ptpython/config.py"
 
 main(){
   mkdir -p ${jupyter_home_path}/host
-  mkdir -p ${jupyter_home_path}/${USER}/.jupyter/lab/user-settings
-  mkdir -p ${jupyter_home_path}/${USER}/.ipython/profile_default/ipython_config.py
+  mkdir -p ${jupyter_home_path}/jupyter/.jupyter/lab/user-settings
+  mkdir -p ${jupyter_home_path}/jupyter/.ipython/profile_default
+  mkdir -p ${jupyter_home_path}/.config/ptpython
 
   case "${input_val}" in
     "down" ) docker-compose down ;;
